05a15410574f5cb21d03875542ac5139e4a9f6cb
Scottyf4 <52453832+Scottyf4@users.noreply.github.com>
2024-10-02 09:15:42 +0100

    search funtionality and fecth for api added

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-const apiUrl = "http://www.omdbapi.com/?i=tt3896198&apikey=5f5d0368";
+const apiUrl = "http://www.omdbapi.com/?apikey=5f5d0368&";
+
+const searchBtn = document.getElementById("searchBtn");
+
+searchBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  let searchValue = document.getElementById("searchInput").value;
+  console.log(searchValue);
+  fetch(`http://www.omdbapi.com/?apikey=5f5d0368&s=${searchValue}`)
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+});
