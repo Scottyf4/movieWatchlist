0c6f8e63d59f9283672b0df18d81d6f1f70d266b
Scottyf4 <52453832+Scottyf4@users.noreply.github.com>
2024-10-02 22:47:17 +0100

    added search functionality and research through imdb

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const apiUrl = "http://www.omdbapi.com/?apikey=5f5d0368&";
+const displayArea = document.getElementById("displayContainer");
 
 const searchBtn = document.getElementById("searchBtn");
 
@@ -8,5 +8,29 @@ searchBtn.addEventListener("click", (e) => {
   console.log(searchValue);
   fetch(`http://www.omdbapi.com/?apikey=5f5d0368&s=${searchValue}`)
     .then((res) => res.json())
-    .then((data) => console.log(data));
+    .then((data) => {
+      if (data.Response === "True") {
+        data.Search.forEach((movie) => {
+          getImbdDetails(movie.imdbID);
+        });
+      } else {
+        console.log(data);
+        displayArea.innerHTML = `
+        <h2>${data.Error}</h2>
+        `;
+      }
+    });
 });
+
+// function to take base details from OMDb and get extra details using the Imdb ID
+function getImbdDetails(id) {
+  fetch(`http://www.omdbapi.com/?apikey=5f5d0368&i=${id}`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      displayArea.innerHTML += `
+        <img src=${data.Poster}/>
+      
+      `;
+    });
+}
