6d2878092fe87282b4e0dc4f8c645868554b7a79
Scottyf4 <52453832+Scottyf4@users.noreply.github.com>
2024-10-06 15:49:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,27 +9,25 @@ const modalMessage = document.getElementById("modalMessage");
 
 // Event listeners for index.html
 
-if (window.location.pathname.endsWith("index.html")) {
-  searchBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    startupDisplay.style.display = "none";
-    displayArea.innerHTML = "";
-    let searchValue = document.getElementById("searchInput").value;
-    getOmdbData(searchValue);
-    searchText.value = "";
-  });
+searchBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  startupDisplay.style.display = "none";
+  displayArea.innerHTML = "";
+  let searchValue = document.getElementById("searchInput").value;
+  getOmdbData(searchValue);
+  searchText.value = "";
+});
 
-  displayArea.addEventListener("click", function (event) {
-    if (event.target.closest(".add-movie-btn")) {
-      const button = event.target.closest(".add-movie-btn");
-      const movieId = button.id.replace("addToWatchlist", "");
+displayArea.addEventListener("click", function (event) {
+  if (event.target.closest(".add-movie-btn")) {
+    const button = event.target.closest(".add-movie-btn");
+    const movieId = button.id.replace("addToWatchlist", "");
 
-      addToWatch(movieId);
-    }
-  });
+    addToWatch(movieId);
+  }
+});
 
-  watchLink.addEventListener("click", displayMovies);
-}
+watchLink.addEventListener("click", displayMovies);
 
 // Function to get data from Omdb
 
@@ -138,26 +136,25 @@ function displayAddedModal(movie) {
 
 // Seperation of concerns for watchlist.html inc functions and event listeners
 
-if (window.location.pathname.endsWith("watchlist.html")) {
-  const watchlistMovies = document.getElementById("watchlistMovies");
+const watchlistMovies = document.getElementById("watchlistMovies");
 
-  displayMovies();
-  // function to display movies on watchlist page from local storage
-  function displayMovies() {
-    const storedMovies = localStorage.getItem("movies");
-    const moviesStored = JSON.parse(storedMovies);
+displayMovies();
+// function to display movies on watchlist page from local storage
+function displayMovies() {
+  const storedMovies = localStorage.getItem("movies");
+  const moviesStored = JSON.parse(storedMovies);
 
-    if (moviesStored.length === 0) {
-      watchlistMovies.innerHTML = `
+  if (moviesStored.length === 0) {
+    watchlistMovies.innerHTML = `
           <section class='emptySection'>
             <img class='magnifyWatchIcon' src='images/largeSearch.png' />
             <h4 class='emptyWatchlist'>No movies to display!</h4>
             <p>Search for movies for your watchlist</p>
           </section>
         `;
-    } else {
-      moviesStored.forEach((movie) => {
-        watchlistMovies.innerHTML += `
+  } else {
+    moviesStored.forEach((movie) => {
+      watchlistMovies.innerHTML += `
         <div class='movie-card'>
           <img class='movie-poster' src=${movie.Poster}/>
           <div>
@@ -181,29 +178,28 @@ if (window.location.pathname.endsWith("watchlist.html")) {
           </div>
         </div>
         `;
-      });
-    }
+    });
   }
-  // eventlisteners to remove movie from watchlist
-  watchlistMovies.addEventListener("click", function (event) {
-    if (event.target.closest(".remove-movie-btn")) {
-      const button = event.target.closest(".remove-movie-btn");
-      const movieId = button.id.replace("removeFromWatchlist", "");
+}
+// eventlisteners to remove movie from watchlist
+watchlistMovies.addEventListener("click", function (event) {
+  if (event.target.closest(".remove-movie-btn")) {
+    const button = event.target.closest(".remove-movie-btn");
+    const movieId = button.id.replace("removeFromWatchlist", "");
 
-      removeMovie(movieId);
-    }
-  });
-  // function to remove movie from watchlist and local storage
-  function removeMovie(movieId) {
-    const localMovies = localStorage.getItem("movies");
-    const returnedMovies = JSON.parse(localMovies);
-
-    const updatedMovies = returnedMovies.filter(
-      (movie) => movie.imdbID !== movieId
-    );
-
-    localStorage.setItem("movies", JSON.stringify(updatedMovies));
-    watchlistMovies.innerHTML = "";
-    displayMovies();
+    removeMovie(movieId);
   }
+});
+// function to remove movie from watchlist and local storage
+function removeMovie(movieId) {
+  const localMovies = localStorage.getItem("movies");
+  const returnedMovies = JSON.parse(localMovies);
+
+  const updatedMovies = returnedMovies.filter(
+    (movie) => movie.imdbID !== movieId
+  );
+
+  localStorage.setItem("movies", JSON.stringify(updatedMovies));
+  watchlistMovies.innerHTML = "";
+  displayMovies();
 }
